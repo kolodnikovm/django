0a994d79a7570eed455c7205c858a8a419909e8f
Kolodnikov Maxim <bredor419@gmail.com>
2018-01-09 21:37:47 +0700

    Registration v.0.1

--- appdir/flickstagram/urls.py
+++ appdir/flickstagram/urls.py
@@ -1,7 +1,11 @@
 from django.urls import path
 
+from django.contrib.auth import views as auth_views
 from . import views
 
 urlpatterns = [
     path('', views.main, name='main'),
+    path('login/', views.login, name='login'),
+    path('register/', views.register, name='register'),
+    path('success_reg/', views.success_reg, name='success_reg'),
 ]
